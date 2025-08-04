01edd02cd46bf982fd1d0fef000fdbfeb2afbd48
The Grey Cardinal <mkctgctg@mail.ru>
2025-08-04 19:15:43 +0400

    init + keywords.

---
+++ Fluence/FluenceKeywords.cs
@@ -0,0 +1,31 @@
+﻿namespace Testing_Chamber.Fluid
+{
+    internal static class FluenceKeywords
+    {
+        private readonly static string[] _keyWords =
+        {
+            "break",
+            "continue",
+            "if",
+            "else",
+            "for",
+            "while",
+            "loop",
+            "in",
+            "func",
+            "nil",
+            "return",
+            "true",
+            "false",
+            "is",
+            "not",
+            "space",
+            "use",
+            "type",
+            "struct",
+            "enum"
+        };
+
+        internal static bool IsAKeyword( string key ) => _keyWords.Contains( key );
+    }
+}

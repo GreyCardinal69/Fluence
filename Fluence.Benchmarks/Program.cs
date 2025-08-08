3f0612bdd814f46976345044c0b47760eb9708bf
The Grey Cardinal <mkctgctg@mail.ru>
2025-08-08 16:34:36 +0400

    feat: Add Fluence Benchmarks project.

---
+++ Fluence.Benchmarks/Program.cs
@@ -0,0 +1,18 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Fluence.Benchmarks
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BenchmarkDotNet.Running.BenchmarkRunner.Run<Benchmarker>();
+            Console.ReadLine();
+        }
+    }
+
+    [MemoryDiagnoser]
+    public class Benchmarker
+    {
+    }
+}

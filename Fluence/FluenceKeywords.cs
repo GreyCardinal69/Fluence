dc509c1a3e565c890cc99d3349f953fcb4d8e13a
The Grey Cardinal <mkctgctg@mail.ru>
2025-08-10 19:01:51 +0400

    Add ReadOnlySpan keyword check method.

--- Fluence/FluenceKeywords.cs
+++ Fluence/FluenceKeywords.cs
@@ -87,6 +87,51 @@ namespace Fluence
                 _ => false,
             };
 
+        internal static TokenType GetKeywordType(ReadOnlySpan<char> text)
+        {
+            switch (text.Length)
+            {
+                case 2:
+                    if (text.SequenceEqual("if")) return TokenType.IF;
+                    if (text.SequenceEqual("in")) return TokenType.IN;
+                    if (text.SequenceEqual("is")) return TokenType.IS;
+                    break;
+                case 3:
+                    if (text.SequenceEqual("for")) return TokenType.FOR;
+                    if (text.SequenceEqual("nil")) return TokenType.NIL;
+                    if (text.SequenceEqual("not")) return TokenType.NOT;
+                    if (text.SequenceEqual("use")) return TokenType.USE;
+                    break;
+                case 4:
+                    if (text.SequenceEqual("else")) return TokenType.ELSE;
+                    if (text.SequenceEqual("enum")) return TokenType.ENUM;
+                    if (text.SequenceEqual("func")) return TokenType.FUNC;
+                    if (text.SequenceEqual("loop")) return TokenType.LOOP;
+                    if (text.SequenceEqual("rest")) return TokenType.REST;
+                    if (text.SequenceEqual("self")) return TokenType.SELF;
+                    if (text.SequenceEqual("true")) return TokenType.TRUE;
+                    if (text.SequenceEqual("type")) return TokenType.TYPE;
+                    break;
+                case 5:
+                    if (text.SequenceEqual("break")) return TokenType.BREAK;
+                    if (text.SequenceEqual("false")) return TokenType.FALSE;
+                    if (text.SequenceEqual("match")) return TokenType.MATCH;
+                    if (text.SequenceEqual("space")) return TokenType.SPACE;
+                    if (text.SequenceEqual("while")) return TokenType.WHILE;
+                    break;
+                case 6:
+                    if (text.SequenceEqual("return")) return TokenType.RETURN;
+                    if (text.SequenceEqual("struct")) return TokenType.STRUCT;
+                    break;
+                case 8:
+                    if (text.SequenceEqual("continue")) return TokenType.CONTINUE;
+                    break;
+            }
+
+            // If we fall through the switch, it's not a keyword.
+            return TokenType.IDENTIFIER;
+        }
+
         internal static bool IsAKeyword(string key) => _keywords.Contains(key);
 
         internal static TokenType GetTokenTypeFromKeyword(string key) =>

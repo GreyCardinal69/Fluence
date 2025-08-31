569917e42154f91d63af7cfe80c4a486a3cfab17
The Grey Cardinal <mkctgctg@mail.ru>
2025-08-31 19:51:40 +0400

    uh oh.

--- Program.cs
+++ Program.cs
@@ -11,9 +11,6 @@
             if (fluenceInterpreter.Compile(source, true))
             {
                 fluenceInterpreter.RunFor(TimeSpan.FromSeconds(30));
-                Console.WriteLine(fluenceInterpreter.GetGlobal("bullshit"));
-                fluenceInterpreter.SetGlobal("bullshit", 10000);
-                Console.WriteLine(fluenceInterpreter.GetGlobal("bullshit"));
             }
         }
     }

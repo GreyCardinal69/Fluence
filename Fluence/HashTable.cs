104056ee8c80c5fa2ed4d5183262413dd1269229
The Grey Cardinal <mkctgctg@mail.ru>
2025-10-11 05:45:03 +0400

    Remove redundant dictionary.

--- Fluence/HashTable.cs
+++ Fluence/HashTable.cs
@@ -2,7 +2,6 @@
 {
     internal static class HashTable
     {
-        private static readonly Dictionary<string, int> _nameToHash = new();
         private static readonly Dictionary<int, string> _hashToName = new();
         private static readonly object _lock = new();
 
@@ -10,16 +9,10 @@
         {
             lock (_lock)
             {
-                if (!_nameToHash.ContainsKey(name))
-                    _nameToHash[name] = hash;
-
                 if (!_hashToName.ContainsKey(hash))
                     _hashToName[hash] = name;
             }
         }
-
-        internal static bool TryGetHash(string name, out int hash)
-            => _nameToHash.TryGetValue(name, out hash);
 
         internal static bool TryGetName(int hash, out string name)
             => _hashToName.TryGetValue(hash, out name!);

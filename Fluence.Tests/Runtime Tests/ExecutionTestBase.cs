8ecfe3f152e3064d432475b6f2568a4723152744
The Grey Cardinal <mkctgctg@mail.ru>
2025-12-04 01:40:34 +0400

    Add runtime tests base.

---
+++ Fluence.Tests/Runtime Tests/ExecutionTestBase.cs
@@ -0,0 +1,69 @@
+﻿using Fluence;
+using Fluence.Exceptions;
+
+namespace Fluence.RuntimeTests
+{
+    public abstract class ExecutionTestBase
+    {
+        public enum FluenceTestType
+        {
+            Integer,
+            Long,
+            Float,
+            Double,
+            String,
+            Char,
+            Bool,
+            Nil
+        }
+
+        public static void AssertScriptResult(string scriptBody, object expectedValue, FluenceTestType expectedType)
+        {
+            var interpreter = new FluenceInterpreter();
+            interpreter.Configuration.OptimizeByteCode = true;
+
+            string fullSource = $@"
+                    result = nil; 
+                    func Main() => {{
+                        {scriptBody}
+                    }}
+                ";
+
+            if (!interpreter.Compile(fullSource))
+                throw new FluenceException("Compilation failed.");
+
+            try
+            {
+                interpreter.RunUntilDone();
+            }
+            catch (FluenceException ex)
+            {
+                throw new FluenceException($"Runtime Error: {ex.Message}");
+            }
+
+            object? actualRaw = interpreter.GetGlobal("result");
+
+            if (expectedType == FluenceTestType.Nil)
+            {
+                Assert.Null(actualRaw);
+                return;
+            }
+
+            Assert.NotNull(actualRaw);
+
+            object convertedActual = expectedType switch
+            {
+                FluenceTestType.Integer => Convert.ToInt32(actualRaw),
+                FluenceTestType.Long => Convert.ToInt64(actualRaw),
+                FluenceTestType.Float => Convert.ToSingle(actualRaw),
+                FluenceTestType.Double => Convert.ToDouble(actualRaw),
+                FluenceTestType.String => Convert.ToString(actualRaw),
+                FluenceTestType.Char => Convert.ToChar(actualRaw),
+                FluenceTestType.Bool => Convert.ToBoolean(actualRaw),
+                _ => throw new ArgumentOutOfRangeException(nameof(expectedType))
+            };
+
+            Assert.Equal(expectedValue, convertedActual);
+        }
+    }
+}
---
+++ Fluence.Tests/Runtime Tests/RuntimeTests.cs
@@ -0,0 +1,19 @@
+﻿using static Fluence.RuntimeTests.ExecutionTestBase;
+
+namespace Fluence.RuntimeTests
+{
+    public class RuntimeTests
+    {
+        [Fact]
+        public void Optimizer_StrengthReduction_Modulo()
+        {
+            AssertScriptResult("result = 10 % 2;", 0, FluenceTestType.Integer);
+        }
+
+        [Fact]
+        public void Optimizer_StrengthReduction_Division()
+        {
+            AssertScriptResult("result = 10 / 2;", 5, FluenceTestType.Integer);
+        }
+    }
+}
